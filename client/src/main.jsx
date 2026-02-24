644d63ae22c35f2177e5ece6c7ec43b5df20e16a
jamesjonathantossou-ayayi <jonathanelie007@gmail.com>
2026-02-24 13:56:53 +0000

    feat(analytics): track GA4 page_view on SPA route changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,36 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const GA_MEASUREMENT_ID = 'G-ESE5C2PLME'
+
+function trackPageView() {
+  if (typeof window.gtag !== 'function') return
+  window.gtag('event', 'page_view', {
+    page_path: window.location.pathname + window.location.search,
+    page_location: window.location.href,
+    send_to: GA_MEASUREMENT_ID,
+  })
+}
+
+if (import.meta.env.PROD) {
+  window.addEventListener('load', trackPageView)
+
+  const originalPushState = window.history.pushState
+  const originalReplaceState = window.history.replaceState
+
+  window.history.pushState = function pushStatePatched(...args) {
+    originalPushState.apply(this, args)
+    trackPageView()
+  }
+
+  window.history.replaceState = function replaceStatePatched(...args) {
+    originalReplaceState.apply(this, args)
+    trackPageView()
+  }
+
+  window.addEventListener('popstate', trackPageView)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
